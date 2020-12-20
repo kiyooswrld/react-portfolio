af5b17524273e85bffcc0fafcf804365dba65732
Kiyoo <zaidahsd9991@gmail.com>
2020-12-20 08:16:28 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Todos from "./components/Todos"
+import Particles from 'react-particles-js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class="outer-wrapper">
+        <div class="wrapper">
+          <div class="slide one">
+          <Particles
+              params={{
+                "particles": {
+                    "number": {
+                        "value": 50
+                    },
+                    "size": {
+                        "value": 3
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        }
+                    }
+                }
+            }} />
+            <Todos />
+          </div>
+          <div class="slide two"></div>
+          <div class="slide three"></div>
+        </div>
+      </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Todos.css"
+
+function Todos() {
+  return (
+    <div>
+     <h1>Hello, I am Kiyoo</h1>
+    </div>
+  );
+}
+
+export default Todos;
