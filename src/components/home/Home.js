9c3150239627153287e7f5a7b701c577063e250f
Kiyoo <zaidahsd9991@gmail.com>
2020-12-21 02:07:55 +0100

    updated first page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,7 +11,7 @@ function Home() {
       <div class="todos">
         <h1>Hello, I <span class="blue">am</span></h1>
         <h2>Kiyoo</h2>
-        <p href="about">Get to know me<ArrowRightAltIcon fontSize="large"/></p>
+        <a href="#about">Get to know me<ArrowRightAltIcon fontSize="large"/></a>
       </div>
     </>
   );
